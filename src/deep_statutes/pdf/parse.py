a2acc61a675dec69413ae5d4ad6dc278177ca3f5
Eric Foley <ericdfoley@gmail.com>
2025-05-05 14:22:30 -0600

    feat: generic pdf parsing

---
+++ src/deep_statutes/pdf/parse.py
@@ -0,0 +1,156 @@
+from pathlib import Path
+
+from lark import Lark, Tree
+
+from deep_statutes.lark import find_matches
+from deep_statutes.pdf.toc import DocumentTOC, Header
+
+
+def _child(tree: Tree, idx: int, name: str) -> Tree:
+    child_tree = tree.children[idx]
+    if child_tree.data != name:
+        raise ValueError(f"Expected {name}, got {child_tree.data}")
+    return child_tree
+
+
+def _text(tree: Tree):
+    """
+    Get all text from the tree's children.
+
+    This assumes that all child nodes are tokens.
+    """
+    t = []
+    for child in tree.children:
+        if isinstance(child, str):
+            t.append(child)
+        else:
+            raise ValueError(f"Expected Token/string but got {child}")
+    return "".join(t)
+
+
+def _to_header(
+    page: int, text_range: tuple[int, int], header_types: list[str], root: Tree
+) -> Header:
+    if root.data != "_header_start":
+        raise ValueError(f'Expected "_header_start", got {root.data}')
+    if len(root.children) != 1:
+        raise ValueError(f"Expected exactly one child, got {len(root.children)}")
+
+    tree = root.children[0]
+
+    if not tree.data.endswith("_start"):
+        raise ValueError(
+            f'Expected grammar rule ending in "_start", got grammar rule "{tree.data}"'
+        )
+
+    header_type = tree.data[:-6]  # remove "_start"
+    if header_type not in header_types:
+        raise ValueError(
+            f'Header type "{header_type}" not in header types "{header_types}"'
+        )
+
+    text = _text(_child(tree, 0, f"{header_type}_number"))
+
+    if len(tree.children) > 1:
+        sub_text = _text(_child(tree, 1, f"{header_type}_subtitle"))
+    else:
+        sub_text = ""
+
+    return Header(
+        type=header_type,
+        text=text,
+        sub_text=sub_text,
+        page=page,
+    )
+
+
+def _child(tree: Tree, idx: int, name: str) -> Tree:
+    child_tree = tree.children[idx]
+    if child_tree.data != name:
+        raise ValueError(f"Expected {name}, got {child_tree.data}")
+    return child_tree
+
+
+def _text(tree: Tree):
+    """
+    Get all text from the tree's children.
+
+    This assumes that all child nodes are tokens.
+    """
+    t = []
+    for child in tree.children:
+        if isinstance(child, str):
+            t.append(child)
+        else:
+            raise ValueError(f"Expected Token/string but got {child}")
+    return "".join(t)
+
+
+example_grammar_not_valid = r"""
+_header_start: title_start | article_start | part_start | section_start
+
+title_start: _SPAN_L title_name _LINE+ title_subtitle
+article_start: _SPAN_M_B article_name _LINE+ article_subtitle
+part_start: _SPAN_M part_name _LINE+ part_subtitle
+
+EOL: /\n/
+
+%ignore EOL
+"""
+
+header_grammar_guidelines = """
+There should be a rule for each header type, and the rule should be named
+<name>_start. 
+
+The rule should contain a child rule named <name>_number, which is the title for the header.
+The rule should optionally contain a child rule named <name>_subtitle, which is the subtitle for the header.
+"""
+
+
+def find_headers(
+    header_grammar: str, header_types: list[str], token_stream_path: Path
+) -> DocumentTOC:
+    """
+    Given a grammar with a "_header_start" rule containing a nested rule for each
+    header type, find all headers in the token stream.
+
+    Args:
+        header_grammar (str): The lark grammar (must be LALR) to use for parsing the headers.
+        header_types (list[str]): The list of header types to find, in hierarchical order.
+        token_stream_path (Path): The path to the token stream file.
+    """
+    lark = Lark(
+        header_grammar,
+        start="_header_start",
+        parser="lalr",
+        propagate_positions=True,
+    )
+
+    headers = []
+    text_idx = 0
+    with open(token_stream_path, "r") as file:
+        text = file.read()
+
+    # keep track of the current page from the token stream
+    page = 0
+    while True:
+        # find next line
+        next_nl_idx = text.find("\n", text_idx + 1)
+        if next_nl_idx == -1:
+            break
+        text_idx = next_nl_idx + 1
+
+        if text[text_idx:].startswith("<<PAGE "):
+            close = text.find(">>", text_idx)
+            page = int(text[text_idx + 7 : close])
+
+        parses, end_pos = find_matches(text[text_idx:], "_header_start", lark)
+
+        if len(parses) >= 1:
+            # get longest possible parse
+            parse = parses[-1]
+            e = end_pos[-1]
+            header = _to_header(page, (text_idx, text_idx + e + 1), header_types, parse)
+            headers.append(header)
+
+    return DocumentTOC(header_types=header_types, headers=headers)
---
+++ src/deep_statutes/pdf/toc.py
@@ -0,0 +1,83 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class Header(BaseModel):
+    type: str
+    text: str
+    sub_text: str
+    page: int  # 1-indexed
+
+
+# TODO rename to HeaderList or something
+class DocumentTOC(BaseModel):
+    header_types: list[str]
+    headers: list[Header]
+
+    def hierarchy_level(self, header_type: str) -> int:
+        """
+        Get the hierarchy level of a header type according to the document's order.
+
+        This may not actually be the order in the document since sometimes headers skip levels, but
+        it gives us a hint as to how to structure a tree representing the ToC.
+        """
+        return self.header_types.index(header_type)
+
+
+class HeaderTreeNode(BaseModel):
+    header: Header
+    parent: Optional["HeaderTreeNode"]
+    children: list["HeaderTreeNode"] = []
+    page_range: tuple[int, int]  # 1-indexed and inclusive
+
+    def num_pages(self) -> int:
+        return self.page_range[1] - self.page_range[0] + 1
+
+    def path(self) -> list["HeaderTreeNode"]:
+        """
+        Get the path of the header in the tree.
+        """
+        path = []
+        node = self
+        while node is not None:
+            path.append(node)
+            node = node.parent
+        path.reverse()
+        return path
+
+    @classmethod
+    def from_toc(cls, toc: DocumentTOC, num_pages: int) -> "HeaderTreeNode":
+        """
+        Build a tree of headers from the TOC.
+        """
+        if toc.headers[0].page == 0:
+            # TODO EDF this is a hack to make sure the first header is on page 1
+            # should just name everything "page_idx" to make clear it's 0-indexed
+            for header in toc.headers:
+                header.page += 1
+
+        # TODO EDF not a big deal if this isn't true, but if so it is kind of weird and
+        # we need a "doc" level header
+        assert toc.headers[0].page == 1, (
+            f"First header must be on page 1 but got {toc.headers[0].page}"
+        )
+        root = HeaderTreeNode(
+            header=toc.headers[0], parent=None, page_range=(1, num_pages)
+        )
+        path: list[HeaderTreeNode] = [root]
+        for header in toc.headers[1:]:
+            level = toc.hierarchy_level(header.type)
+            while toc.hierarchy_level(path[-1].header.type) >= level:
+                # this could happen if there isn't a toc entry for "this entire document"
+                # we could deal with that if it occurs by always putting in a header at "document" level
+                # or something
+                assert len(path) > 1
+
+                path[-1].page_range = (path[-1].page_range[0], header.page)
+                path.pop()
+            node = HeaderTreeNode(
+                header=header, parent=path[-1], page_range=[header.page, num_pages]
+            )
+            path[-1].children.append(node)
+            path.append(node)
+        return root
